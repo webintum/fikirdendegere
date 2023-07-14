1ed9da6d73d4cb64aa3a3744cfd6ce40af9e067c
webintum <webintum@gmail.com>
2023-07-14 17:48:07 +0300

    header değiişiklik

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
 <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
   <Header>
-    Header
+   <div className="mb-2">
+    <h1 className="text-white text-3xl font-semibold">
+      Tekrar Hoşgeldin
+    </h1>
+   </div>
   </Header>
 </div>
   )
--- components/Button.tsx
+++ components/Button.tsx
@@ -13,11 +13,16 @@ interface ButtomProps
         ...props 
     }, ref) => {
         return (
-            <button type={type} className={twMerge(`w-full rounded-full bg-teal-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`)}>
+            <button type={type} className={twMerge(`w-full rounded-full bg-teal-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`, className
+            )}
+            disabled={disabled}
+            ref={ref}
+            {...props}
+            >
                 {children}
             </button>
         )
     })    
 
- Button.displayName = "Button";
+Button.displayName = "Button";
 export default Button;
